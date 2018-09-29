d5ad89b370684114cefac72c96f417a0dc4d604c
Judyzzzou <judyzou666@gmail.com>
2018-09-29 22:36:18 +1000

    add title in help parent page

--- src/containers/help_parents.js
+++ src/containers/help_parents.js
@@ -19,7 +19,8 @@ class HelpParents extends Component {
 
     renderParentCard(cards) {
         return cards.map(card => {
-            return <img key={card.id} onClick={() => this.showTooltips(card.kidDialog, card.parentDialog, card.kidUrl)} style={{cursor: 'pointer'}} src={require(`../static/help_your_parents/Card_0${card.id}.svg`)} alt="parent card" />
+            return <img key={card.id} onClick={() => this.showTooltips(card.kidDialog, card.parentDialog, card.kidUrl)} 
+                    style={{cursor: 'pointer'}} src={require(`../static/help_your_parents/Card_0${card.id}.svg`)} alt="parent card" />
         });
     }
 
@@ -65,18 +66,18 @@ class HelpParents extends Component {
         return (
             <div style={{ width: 1000 }}>
                 <h4 style={{color:"white"}}>Help your parents in daily life with suggestions! Click images at bottom!</h4>
-                <div style={{ flex: 1, display: 'flex', justifyContent: 'space-evenly' }}>
+                <div style={{ marginTop:34,  flex: 1, display: 'flex', justifyContent: 'space-evenly' }}>
                     <Tooltip
                         visible={this.state.kidTooltipVisible}
                         title={this.renderTooltipTitle(this.state.kidTooltipTitle, this.state.kidTooltipUrl)}
-                        placement="rightTop" 
+                        placement="right" 
                         overlayClassName="helpParentstooltip">
                         <img src={require('../static/help_your_parents/child.svg')} alt="reusable straws" />
                     </Tooltip>
                     <Tooltip
                         visible={this.state.parentTooltipVisible}
                         title={this.renderTooltipTitle(this.state.parentTooltipTitle, null)}
-                        placement="rightTop" 
+                        placement="right" 
                         overlayClassName="helpParentstooltip">
                         <img src={require('../static/help_your_parents/mom.svg')} alt="reusable bags" />
                     </Tooltip>
