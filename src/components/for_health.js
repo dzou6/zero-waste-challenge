49080e8d297565638593b6aafa271988f68a800c
Zeli <ak47m40080@gmail.com>
2018-10-07 16:50:04 +1100

    br
    br

--- src/components/for_health.js
+++ src/components/for_health.js
@@ -86,7 +86,7 @@ class health extends Component {
           <button id="cardiovascular" onClick={this.onClickCardiovascular}>btn_02</button>
         </div>
           <div id="diabetes_p">
-             Look out!<br/> using plastic product for more than <br/><div id="d"></div> days, may double your chances<br/>of getting diabetes.<br/>
+             Look out!<br/> using plastic product for more than <div id="d"></div> days, may double your chances<br/>of getting diabetes.<br/>
              <a className="readmore" href="https://academic.oup.com/jcem/article/96/12/3822/2834949"><em>Read more</em></a>
           </div>
           <div id="heart_warning">
